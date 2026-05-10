6fc760eb6ff6197c5b6a8a80657ae3d284d83191
Jhon Mikel <jhonmikelfelipe20@gmail.com>
2026-05-10 23:55:44 +0800

    Add build script for NLTK and spaCy data

---
+++ build.sh
@@ -0,0 +1,13 @@
+﻿#!/bin/bash
+# Install system dependencies
+apt-get update
+apt-get install -y gcc python3-dev default-libmysqlclient-dev build-essential
+
+# Install Python packages
+pip install -r requirements.txt
+
+# Download NLTK data
+python -c "import nltk; nltk.download('punkt'); nltk.download('stopwords'); nltk.download('vader_lexicon'); nltk.download('words')"
+
+# Download spaCy model
+python -m spacy download en_core_web_sm
